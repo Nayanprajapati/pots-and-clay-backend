813276cbb0b259b3a0bb6aa677f91c90426aa7b3
Nayanprajapati <nayanpraz123@gmail.com>
2025-01-31 18:53:55 +0545

    usersverify

--- controller/VerifyOtpControllers.js
+++ controller/VerifyOtpControllers.js
@@ -1,23 +1,23 @@
-const User = require("../models/userModels");
+// const User = require("../models/userModels");
 
-// Verify OTP controller function
-const verifyOtp = async (req, res) => {
-  const { email, otp } = req.body;
-  const user = await User.findOne({ email, otp });
+// // Verify OTP controller function
+// const verifyOtp = async (req, res) => {
+//   const { email, otp } = req.body;
+//   const user = await User.findOne({ email, otp });
 
-  if (!user || user.otpExpiry < Date.now()) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Invalid or expired OTP" });
-  }
+//   if (!user || user.otpExpiry < Date.now()) {
+//     return res
+//       .status(400)
+//       .json({ success: false, message: "Invalid or expired OTP" });
+//   }
 
-  user.otp = null;
-  user.otpExpiry = null;
-  await user.save();
+//   user.otp = null;
+//   user.otpExpiry = null;
+//   await user.save();
 
-  res.status(200).json({ success: true, message: "OTP verified successfully" });
-};
+//   res.status(200).json({ success: true, message: "OTP verified successfully" });
+// };
 
-module.exports = {
-  verifyOtp,
-};
+// module.exports = {
+//   verifyOtp,
+// };
