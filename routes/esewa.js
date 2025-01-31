3b9b81d740087243fe38352813969c356aa517ff
Nayanprajapati <nayanpraz123@gmail.com>
2025-01-31 18:58:00 +0545

    routesesewas

--- routes/esewa.js
+++ routes/esewa.js
@@ -1,13 +1,13 @@
-// var express = require("express");
-// const {
-//   handleEsewaSuccess,
-//   handleEsewaFailure,
-// } = require("../controller/esewaController");
-// const { createOrder } = require("../controller/esewaController");
-// var router = express.Router();
+var express = require("express");
+const {
+  handleEsewaSuccess,
+  handleEsewaFailure,
+} = require("../controller/esewaController");
+const { createOrder } = require("../controller/esewaController");
+var router = express.Router();
 
-// router.get("/success", handleEsewaSuccess);
-// router.post("/create", createOrder);
-// router.get("/failure", handleEsewaFailure);
+router.get("/success", handleEsewaSuccess);
+router.post("/create", createOrder);
+router.get("/failure", handleEsewaFailure);
 
-// module.exports = router;
+module.exports = router;
