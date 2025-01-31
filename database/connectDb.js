a3c1a60007d86722f6de5f99d23dd0161f105f31
Nayanprajapati <nayanpraz123@gmail.com>
2025-01-31 18:54:37 +0545

    databse

--- database/connectDb.js
+++ database/connectDb.js
@@ -1,12 +1,12 @@
-// importing the package
-const mongoose = require("mongoose");
+// // importing the package
+// const mongoose = require("mongoose");
 
-// creating a function
-const connectDB = () => {
-  mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log("database connected");
-  });
-};
+// // creating a function
+// const connectDB = () => {
+//   mongoose.connect(process.env.MONGO_URI).then(() => {
+//     console.log("database connected");
+//   });
+// };
 
-// exporting the function
-module.exports = connectDB;
+// // exporting the function
+// module.exports = connectDB;
