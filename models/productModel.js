b63843b11c43605ce029b7b2bd5693b96c05b137
Nayanprajapati <nayanpraz123@gmail.com>
2025-01-31 18:55:57 +0545

    productm

--- models/productModel.js
+++ models/productModel.js
@@ -1,34 +1,34 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  // fields : NAME, PRICE, DESCRIPTION, IMAGE, CATEGORY
-  productName: {
-    type: String,
-    required: true,
-  },
-  productPrice: {
-    type: Number,
-    required: true,
-  },
-  productDescription: {
-    type: String,
-    required: true,
-    maxlength: 300,
-  },
-  productCategory: {
-    type: String,
-    required: true,
-  },
-  productImage: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+// const productSchema = new mongoose.Schema({
+//   // fields : NAME, PRICE, DESCRIPTION, IMAGE, CATEGORY
+//   productName: {
+//     type: String,
+//     required: true,
+//   },
+//   productPrice: {
+//     type: Number,
+//     required: true,
+//   },
+//   productDescription: {
+//     type: String,
+//     required: true,
+//     maxlength: 300,
+//   },
+//   productCategory: {
+//     type: String,
+//     required: true,
+//   },
+//   productImage: {
+//     type: String,
+//     required: true,
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
 
-// exporting
-const Products = mongoose.model("products", productSchema);
-module.exports = Products;
+// // exporting
+// const Products = mongoose.model("products", productSchema);
+// module.exports = Products;
