ef4d87f41d51f5ed3597c20acca781a3a8309a46
Nayanprajapati <nayanpraz123@gmail.com>
2025-01-31 18:52:27 +0545

    pass

--- controller/resetPasswordControllers.js
+++ controller/resetPasswordControllers.js
@@ -1,44 +1,44 @@
-const User = require("../models/userModels");
-const bcrypt = require("bcryptjs");
-const { generatePassword } = require("./userControllers");
+// const User = require("../models/userModels");
+// const bcrypt = require("bcryptjs");
+// const { generatePassword } = require("./userControllers");
 
-// Reset Password controller function
-const resetPassword = async (req, res) => {
-  try {
-    const { email, newPassword } = req.body;
-    console.log("Received data:", req.body);
+// // Reset Password controller function
+// const resetPassword = async (req, res) => {
+//   try {
+//     const { email, newPassword } = req.body;
+//     console.log("Received data:", req.body);
 
-    if (!email || !newPassword) {
-      return res.status(400).json({
-        success: false,
-        message: "Email and new password are required",
-      });
-    }
+//     if (!email || !newPassword) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "Email and new password are required",
+//       });
+//     }
 
-    const user = await User.findOne({ email });
+//     const user = await User.findOne({ email });
 
-    if (!user) {
-      return res
-        .status(400)
-        .json({ success: false, message: "User not found" });
-    }
+//     if (!user) {
+//       return res
+//         .status(400)
+//         .json({ success: false, message: "User not found" });
+//     }
 
-    const { salt, hash } = generatePassword(newPassword);
+//     const { salt, hash } = generatePassword(newPassword);
 
-    user.salt = salt;
-    user.password = hash;
+//     user.salt = salt;
+//     user.password = hash;
 
-    await user.save();
+//     await user.save();
 
-    res
-      .status(200)
-      .json({ success: true, message: "Password reset successfully" });
-  } catch (error) {
-    console.error("Error resetting password:", error);
-    res.status(500).json({ success: false, message: "Internal server error" });
-  }
-};
+//     res
+//       .status(200)
+//       .json({ success: true, message: "Password reset successfully" });
+//   } catch (error) {
+//     console.error("Error resetting password:", error);
+//     res.status(500).json({ success: false, message: "Internal server error" });
+//   }
+// };
 
-module.exports = {
-  resetPassword,
-};
+// module.exports = {
+//   resetPassword,
+// };
